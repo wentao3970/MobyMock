edd4574261c64b2cef7a97f17d84b1c90f8002db
Wentao Wu <wentao3970@gmail.com>
2021-01-22 13:54:17 -0500

    Initial Commit

---
+++ MobyMock/MobyMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobyMockApp.swift
+//  MobyMock
+//
+//  Created by Wentao on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MobyMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
